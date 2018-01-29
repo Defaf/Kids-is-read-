9ed23baf118c31106171f8ef31da5886b2e34c4c
Defaf <defaf_kamfar@hotmail.com>
2018-01-29 15:46:16 +0300

    Add files via upload

---
+++ Kids is read/css/sharedraw.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!--[if lte IE 8]> <html class="oldie" lang="en"> <![endif]-->
+<!--[if IE 9]> <html class="ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<?php include "projectheader.php"; ?>
+</head>
+<body>	
+			
+		<?php include "projectfooter.php"; ?>
+</body>
+</html>
